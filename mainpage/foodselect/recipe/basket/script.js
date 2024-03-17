3f4b9ee34b54e03c1cd5eb7f0e95dfd467940bd4
DDH-975 <131092088+DDH-975@users.noreply.github.com>
2024-03-17 20:14:07 +0900

    뒤로가기 버튼 추가

--- mainpage/foodselect/recipe/basket/script.js
+++ mainpage/foodselect/recipe/basket/script.js
@@ -6,15 +6,24 @@
         return;
     }
 
+
+//뒤로가기 버튼 
+    function goBack() {
+        window.history.back();
+      }
+
+      
+
     var selectedProductIds = [];
     checkedProducts.forEach(function(checkbox) {
         selectedProductIds.push(checkbox.parentNode.querySelector('h2').textContent);
     });
 
-    // 선택된 상품들의 ID나 다른 정보를 가져와 다음 페이지로 전달할 수 있습니다.
-    // 여기서는 간단한 alert로 대신합니다.
+    // 선택된 상품들의 ID나 다른 정보를 가져와 다음 페이지로 전달
     alert("선택된 상품: " + selectedProductIds.join(", "));
 
-    // 여기서 다음 페이지로 이동하도록 설정하십시오.
+
+
+    // 결제 페이지가 들어갈 코드 
     // window.location.href = '결제 페이지 URL';
 }
